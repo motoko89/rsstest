2833588bee0cc0447b8f38608b1fb05559d43aae
Hung H Ho <hohoanghung2010@gmail.com>
2019-11-09 19:09:19 -0800

    init proj

---
+++ RSSPlayground/RSSPlayground/MainPage.xaml.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Plugin.Media;
+using Plugin.Media.Abstractions;
+using Plugin.Permissions;
+using Plugin.Permissions.Abstractions;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace RSSPlayground
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        private static int imgCount = 1;
+        public static List<string> StatTexts = new List<string>();
+
+        private double width;
+        private double height;
+        private double density;
+
+        private string manufacturer;
+        private string model;
+        private string platform;
+        private string idiom;
+        private string deviceType;
+        private string osVersion;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            var mainDisplayInfo = DeviceDisplay.MainDisplayInfo;
+            width = mainDisplayInfo.Width;
+            height = mainDisplayInfo.Height;
+            density = mainDisplayInfo.Density;
+            manufacturer = DeviceInfo.Manufacturer;
+            model = DeviceInfo.Model;
+            platform = Device.RuntimePlatform;
+            idiom = DeviceInfo.Idiom.ToString();
+            deviceType = DeviceInfo.DeviceType.ToString();
+            osVersion = DeviceInfo.VersionString;
+            DeviceInfoLabel.Text = "Screensize: W" + width + " x H" + height + ". Density " + density + Environment.NewLine + Environment.NewLine +
+                        "Device information: " + manufacturer + " " + model + ". " + platform + " " + idiom + " " + deviceType + ". OS version " + osVersion;
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            Stats.Text = string.Join(Environment.NewLine, StatTexts);
+        }
+
+        private void TakePhoto_OnClicked(object sender, EventArgs e)
+        {
+            Device.BeginInvokeOnMainThread(async () => await TakePhotoUI());
+        }
+
+        private async Task TakePhotoUI()
+        {
+            if (await EnsurePermissions())
+            {
+                MediaFile file = await CrossMedia.Current.TakePhotoAsync(
+                    new StoreCameraMediaOptions
+                    {
+                        MaxWidthHeight = 1200,
+                        SaveToAlbum = false,
+                        PhotoSize = PhotoSize.MaxWidthHeight,
+                        CompressionQuality = 100,
+                        AllowCropping = false
+                    });
+                if (file != null)
+                {
+                    string newLine = imgCount + " Taken photo size " + (file.GetStream()?.Length.ToString() ?? "-1");
+                    imgCount++;
+                    StatTexts.Add(newLine);
+                    Stats.Text += Environment.NewLine + newLine;
+                    Navigation.PushAsync(new PhotoEditPage(file));
+                }
+            }
+            else
+            {
+                await DisplayAlert("PermissionDenied", "InsufficientPermissions", "OK");
+            }
+        }
+
+        private async Task<bool> EnsurePermissions()
+        {
+            var cameraStatus = await CrossPermissions.Current.CheckPermissionStatusAsync(Permission.Camera);
+            var photoStatus = await CrossPermissions.Current.CheckPermissionStatusAsync(Permission.Photos);
+            var storageStatus = await CrossPermissions.Current.CheckPermissionStatusAsync(Permission.Storage);
+            if (cameraStatus != PermissionStatus.Granted ||
+                storageStatus != PermissionStatus.Granted ||
+                photoStatus != PermissionStatus.Granted)
+            {
+                Debug.WriteLine("Request permissions");
+                var results = await CrossPermissions.Current.RequestPermissionsAsync(new[] {
+                    Permission.Camera,
+                    Permission.Storage,
+                    Permission.Photos
+                });
+                cameraStatus = results[Permission.Camera];
+                storageStatus = results[Permission.Storage];
+                photoStatus = results[Permission.Photos];
+            }
+
+            Debug.WriteLine("Camera status {0}. Storage status {1}. Photo status {2}", cameraStatus, storageStatus, photoStatus);
+            return cameraStatus == PermissionStatus.Granted &&
+                   storageStatus == PermissionStatus.Granted &&
+                   photoStatus == PermissionStatus.Granted;
+        }
+    }
+}
---
+++ RSSPlayground/RSSPlayground/PhotoEditPage.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using Plugin.Media.Abstractions;
+using Syncfusion.SfImageEditor.XForms;
+using Xamarin.Forms;
+
+namespace RSSPlayground
+{
+    public partial class PhotoEditPage : ContentPage
+    {
+        private readonly MediaFile photo;
+        public PhotoEditPage(MediaFile photo)
+        {
+            InitializeComponent();
+            this.photo = photo;
+            InitPhotoEditor(photo.GetStream());
+        }
+
+        private void InitPhotoEditor(Stream stream)
+        {
+            IPEditor.Source = ImageSource.FromStream(() => stream);
+        }
+
+        private void Handle_ImageSaving(object sender, ImageSavingEventArgs args)
+        {
+            Debug.WriteLine(DateTime.UtcNow + " Image saving");
+            args.Cancel = true;
+            var stream = args.Stream;
+            MainPage.StatTexts.Add("SfImageEditor stream size " + stream?.Length.ToString());
+
+            Navigation.PopAsync();
+            Debug.WriteLine(DateTime.UtcNow + " Image saving done");
+        }
+
+        private void Handle_ImageSaved(object sender, ImageSavedEventArgs args)
+        {
+            string savedLocation = args.Location; // You can get the saved image location with the help of this argument
+            Debug.WriteLine(DateTime.UtcNow + " args.Location = " + args.Location);
+            try
+            {
+                File.Delete(savedLocation);
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+
+        private void SavePhoto_OnClicked(object sender, EventArgs e)
+        {
+            Debug.WriteLine(DateTime.UtcNow + " SavePhoto_OnClicked");
+            Device.BeginInvokeOnMainThread(() =>
+            {
+                try
+                {
+                    Debug.WriteLine(DateTime.UtcNow + " Save thread");
+                    IPEditor.Save(".jpg");
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine(ex.GetType() + ":" + ex.Message + ":" + ex.StackTrace);
+                }
+            });
+            Debug.WriteLine(DateTime.UtcNow + " SavePhoto_OnClicked ends");
+        }
+    }
+}
